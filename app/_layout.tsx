e56983f9388df11e0e39fb7aaeebcfccac5a5676
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-10-21 17:44:23 +0000

    some changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      <Stack.Screen name="modal" options={{ presentation: 'fullScreenModal' }} />
     </Stack>
   );
 }
