a686de1d0e58c8986fcbde4fc86072c4e28ad139
Emmanuel <emmsom506@gmail.com>
2025-10-29 14:34:03 +0000

    Add featured list to the market screen

--- app/(protected)/(drawer)/(tabs)/market-place-screen/index.tsx
+++ app/(protected)/(drawer)/(tabs)/market-place-screen/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import {SafeAreaView} from "react-native-safe-area-context";
-import {Text, View} from "react-native";
+import {ScrollView, StyleSheet} from "react-native";
+import MarketplaceHeader from "@/features/marketplace/marketplace-header";
+import FeaturedList from "@/features/marketplace/featured-list";
 
 const Index = () => {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>
-                    Market Place Screen
-                </Text>
-            </View>
-
-        </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <MarketplaceHeader />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <FeaturedList/>
+        </ScrollView>
+      </SafeAreaView>
     );
 };
-
 export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
