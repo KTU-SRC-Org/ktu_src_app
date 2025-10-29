1e1ded5beaaeed1c1d1d79d2682b23ab40605890
Emmanuel <emmsom506@gmail.com>
2025-10-29 16:16:55 +0000

    New product creation forms

---
+++ features/marketplace/new-product-forms.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native";
+
+const NewProductForms = () => {
+  return(
+    <View>
+      <Text>
+        New Product Forms
+      </Text>
+    </View>
+  )
+}
+export default NewProductForms;
