649476f673639984966ec69f424bfc230deb0415
Emmanuel <emmsom506@gmail.com>
2025-11-06 17:44:21 +0000

    Add events types

---
+++ types/events.types.ts
@@ -0,0 +1,42 @@
+export type TabKeys = "featured" | "popular" | "upcoming"
+
+export interface Event {
+  id: string;
+  title: string;
+  date: Date | string;
+  location: string;
+  category?: string;
+  description?: string;
+  image?: string;
+  attendees?: string;
+  organizer?: string;
+}
+
+export interface BookingsInterface {
+  selectedCanopies: {
+    id: string;
+    name: string;
+    price: number;
+    selectedQty: number;
+  }[];
+  totalAmount: number;
+}
+
+export interface CanopyInterface {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export type CanopyModalProps = {
+  visible: boolean;
+  onClose: () => void;
+  event: Event;
+};
+
+export type ShowSuccessData = {
+  visible: boolean;
+  data?: BookingsInterface
+};
+
