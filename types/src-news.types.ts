316422030d62d53306077ad8393fc87cce861e80
Emmanuel <emmsom506@gmail.com>
2025-11-12 19:53:07 +0000

    Add category type

--- types/src-news.types.ts
+++ types/src-news.types.ts
@@ -1,12 +1,14 @@
 import {ImageSourcePropType} from "react-native";
 
+export type CategoryType = "tech" | 'business';
+
 export interface SrcNewsInterface {
   id: string;
-  title: string;
   body: string;
   publisherImage: ImageSourcePropType;
   publishedAt: Date;
   publisher: string;
+  category: CategoryType[]
   source: string;
   isDraft?: boolean;
   createAt?: string;
